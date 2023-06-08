d8d30251ef31dda6347cefc5d495351b7d1cfa8f
wjd27149 <1397246915@qq.com>
2023-06-08 20:06:28 +0800

    init

---
+++ resnet/predict.py
@@ -0,0 +1,65 @@
+import os
+import json
+
+import torch
+from PIL import Image
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+from model import resnet34
+
+def main():
+    device = torch.device("cuda: 0" if torch.cuda.is_available() else "cpu")
+
+    data_transform = transforms.Compose(
+        [transforms.Resize(256),
+         transforms.CenterCrop(224),
+         transforms.ToTensor(),
+         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+    
+    # load image
+    img_path = "./sunflower.jpg"
+    assert os.path.exists(img_path), "file {} does not exit".format(img_path)
+    img = Image.open(img_path)
+    plt.imshow(img)
+    #[N, C, H, W]----(batch_size channel height width)
+    img =data_transform(img)# 预处理
+    img = torch.unsqueeze(img, dim= 0)# 拓展第一个通道
+
+    #read class_dict
+    json_path = "./class_indices.json"
+    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
+
+    with open(json_path,"r") as f:
+        class_dict = json.load(f)
+
+    print(class_dict)
+    #creat model
+    model = resnet34(num_classes= 5).to(device)
+
+    #load model weights
+    weights_path = "./resNet34.pth"
+    assert os.path.exists(weights_path), "file '{}' does not exits".format(weights_path)
+    model.load_state_dict(torch.load(weights_path, map_location=device))
+
+    # prediction
+    model.eval()
+    with torch.no_grad():
+        #predict class  由于未指定设备，PyTorch 会默认使用 CPU 进行计算
+        output = torch.squeeze(model(img.to(device))).cpu()
+        predict = torch.softmax(output, dim = 0)
+        predict_cla = torch.argmax(predict).numpy()
+    
+    print(predict)
+    print(predict_cla)
+    # class_dict 字典，通过键 查找 值
+    print_res = "class: {}  prob: {: .3}".format(class_dict[str(predict_cla)],predict[predict_cla].numpy())
+    print(print_res)
+    for i in range(len(predict)):
+        print("class: {:10}   prob: {:.3}".format(class_dict[str(i)],
+                                                  predict[i].numpy()))
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ resnet/train.py
@@ -0,0 +1,147 @@
+import os
+import sys
+import json
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms, datasets
+from tqdm import tqdm
+import torchvision.models.resnet
+
+from model import resnet34
+
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("using {} device.".format(device))
+    
+#这段代码是数据预处理中的一部分，它定义了两个数据转换的管道，分别用于训练集和验证集的图像数据预处理。
+# 具体来说，这段代码中包含了四个数据转换操作：
+# 1. RandomResizedCrop：随机裁剪，将图像随机裁剪成指定的大小（这里是224×224），同时还可以进行缩放和裁剪的比例随机变化，这样可以增加数据的多样性，提高模型的泛化能力。
+# 2. RandomHorizontalFlip：随机水平翻转，以一定的概率对图像进行水平翻转，这样可以增加数据的多样性，提高模型的泛化能力。
+# 3. ToTensor：将图像转换成张量形式，这是深度学习中常用的数据格式。
+# 4. Normalize：对图像进行归一化处理，使得图像的像素值在0到1之间，并且均值为0，标准差为1。这样做的目的是为了使得不同的图像数据具有可比性，使得模型更容易学习到图像中的特征。
+# 在这段代码中，对图像进行预处理的具体操作是先进行随机裁剪和水平翻转，然后将图像转换成张量形式，最后进行归一化处理。这些操作可以使得模型更好地学习到图像中的特征，并且提高模型的泛化能力。
+
+    data_transform = {
+        "train": transforms.Compose([transforms.RandomResizedCrop(224),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+        "val": transforms.Compose([transforms.Resize(256),
+                                   transforms.CenterCrop(224),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
+
+    data_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))  # get data root path
+    image_path = os.path.join(data_root, "flower_photos")  # flower data set path
+    assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
+    train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
+                                         transform=data_transform["train"])
+    train_num = len(train_dataset)
+
+    # {'daisy':0, 'dandelion':1, 'roses':2, 'sunflower':3, 'tulips':4}
+    flower_list = train_dataset.class_to_idx
+    cla_dict = dict((val, key) for key, val in flower_list.items())
+    # write dict into json file
+    json_str = json.dumps(cla_dict, indent=4)
+    with open('class_indices.json', 'w') as json_file:
+        json_file.write(json_str)
+
+    batch_size = 16
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    print('Using {} dataloader workers every process'.format(nw))
+
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size, shuffle=True,
+                                               num_workers=nw)
+
+    validate_dataset = datasets.ImageFolder(root=os.path.join(image_path, "val"),
+                                            transform=data_transform["val"])
+    val_num = len(validate_dataset)
+    validate_loader = torch.utils.data.DataLoader(validate_dataset,
+                                                  batch_size=batch_size, shuffle=False,
+                                                  num_workers=nw)
+
+    print("using {} images for training, {} images for validation.".format(train_num,
+                                                                           val_num))
+    
+    net = resnet34()
+    # load pretrain weights
+    # download url: https://download.pytorch.org/models/resnet34-333f7ec4.pth
+    model_weight_path = "./resnet34-pre.pth"
+    assert os.path.exists(model_weight_path), "file {} does not exist.".format(model_weight_path)
+    net.load_state_dict(torch.load(model_weight_path, map_location='cpu'))
+    for param in net.parameters():
+        param.requires_grad = False
+
+    # change fc layer structure
+    in_channel = net.fc.in_features
+    net.fc = nn.Linear(in_channel, 5)
+    net.to(device)
+
+    # define loss function
+    loss_function = nn.CrossEntropyLoss()
+
+    # construct an optimizer
+    params = [p for p in net.parameters() if p.requires_grad]
+    optimizer = optim.Adam(params, lr=0.0001)
+
+    epochs = 1
+    best_acc = 0.0
+    save_path = './resNet34.pth'
+    train_steps = len(train_loader)
+    for epoch in range(epochs):
+        # train
+        net.train()
+        running_loss = 0.0
+        train_bar = tqdm(train_loader, file=sys.stdout)
+        for step, data in enumerate(train_bar):
+            images, labels = data
+            # 对历史梯度进行清零
+            optimizer.zero_grad()
+            logits = net(images.to(device))
+            loss = loss_function(logits, labels.to(device))
+            # 反向传播
+            loss.backward()
+            # 参数的更新
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+
+            train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
+                                                                     epochs,
+                                                                     loss)
+
+        # validate # net.eval()关闭bn层和dropout() ---- 随机失活  net.train() 则会打开
+        net.eval()
+        acc = 0.0  # accumulate accurate number / epoch
+        # 防止内存崩， 不用计算误差梯度
+        with torch.no_grad():
+            val_bar = tqdm(validate_loader, file=sys.stdout)
+            for val_data in val_bar:
+                val_images, val_labels = val_data
+                outputs = net(val_images.to(device))
+                # loss = loss_function(outputs, test_labels)
+                #求出输出中最有可能的类别
+                predict_y = torch.max(outputs, dim=1)[1]
+                acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
+
+                val_bar.desc = "valid epoch[{}/{}]".format(epoch + 1,
+                                                           epochs)
+
+        val_accurate = acc / val_num
+        print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
+              (epoch + 1, running_loss / train_steps, val_accurate))
+
+        if val_accurate > best_acc:
+            best_acc = val_accurate
+            torch.save(net.state_dict(), save_path)
+
+    print('Finished Training')
+
+
+if __name__ == '__main__':
+    main()
